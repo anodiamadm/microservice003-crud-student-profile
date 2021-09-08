d010072a185db590b777028821e67b451708dba7
anodiamadm <anodiamadm@gmail.com>
2021-09-08 11:43:16 +1000

    Delete RoleRepository.java

--- src/main/java/com/anodiam/JWTAuth/db/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anodiam.JWTAuth.db.repository;
-
-import com.anodiam.JWTAuth.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
