34a48774686e1118915722d37c2074fd25b8972a
anodiamadm <anodiamadm@gmail.com>
2021-09-08 11:43:20 +1000

    Create UserPrincipalDetailService.java

---
+++ src/main/java/com/anodiam/CRUDStudentProfile/security/UserPrincipalDetailService.java
@@ -0,0 +1,27 @@
+package com.anodiam.CRUDStudentProfile.security;
+
+import com.anodiam.CRUDStudentProfile.db.repository.UserRepository;
+import com.anodiam.CRUDStudentProfile.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+//        Handle User Not Found exception here *****
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
