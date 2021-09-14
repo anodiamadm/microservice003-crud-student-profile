e5483ac9ddf5d7ab577ae7e471d7e412780e2c38
anodiamadm <anodiamadm@gmail.com>
2021-09-14 15:33:53 +1000

    Update TownRepository.java

--- src/main/java/com/anodiam/CRUDStudentProfile/db/repository/address/TownRepository.java
+++ src/main/java/com/anodiam/CRUDStudentProfile/db/repository/address/TownRepository.java
@@ -1,5 +1,6 @@
 package com.anodiam.CRUDStudentProfile.db.repository.address;
 
+import com.anodiam.CRUDStudentProfile.model.StudentProfile;
 import com.anodiam.CRUDStudentProfile.model.address.Town;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface TownRepository extends JpaRepository<Town, BigInteger> {
 
     List<Town> findByState(BigInteger stateId);
+
 }
