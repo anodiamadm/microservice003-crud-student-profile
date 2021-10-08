143bb405bfd737ad11206274a19e09b8e9e7e910
Debashish Nath <anodiam.dn@gmail.com>
2021-10-08 11:39:00 +0530

    2 Test Cases added
    2 test cases added

--- src/test/java/com/anodiam/CRUDStudentProfile/JwtAuthApplicationTests.java
+++ src/test/java/com/anodiam/CRUDStudentProfile/JwtAuthApplicationTests.java
@@ -1,13 +1,41 @@
 package com.anodiam.CRUDStudentProfile;
 
+import com.anodiam.CRUDStudentProfile.model.address.Country;
+import com.anodiam.CRUDStudentProfile.serviceRepository.address.Country.CountryService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 
 @SpringBootTest
 class JwtAuthApplicationTests {
+
+	@Autowired
+	private CountryService countryService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testString() throws Exception {
+		String expectedValue="HELLOWORLD";
+		String actualValue="HELLOWORLD";
+		assertEquals(expectedValue, actualValue);
+	}
+
+	@Test
+	public void testCountryName() throws Exception {
+		List<Country> actualValue=countryService.findAll();
+		List<Country> expectedValue=new ArrayList<Country>();
+		BigInteger big_integer = BigInteger.valueOf(1);
+		expectedValue.add(new Country(big_integer,"AU","Australia"));
+		assertEquals(expectedValue.get(0).getCountry_name(), actualValue.get(0).getCountry_name());
+	}
 }
